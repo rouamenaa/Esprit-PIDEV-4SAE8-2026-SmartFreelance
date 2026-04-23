ef846913b295b6a8cc75d05235f6b0f3685a2e66
nassim-rf <Nassim.Rafrafi@esprit.tn>
2026-04-23 13:11:09 +0100

    Update PiApplicationTests.java

--- smartfreelance-backend/freelancer-service/src/test/java/com/esprit/microservice/pi/PiApplicationTests.java
+++ smartfreelance-backend/freelancer-service/src/test/java/com/esprit/microservice/pi/PiApplicationTests.java
@@ -1,13 +1,1 @@
-package com.esprit.microservice.pi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
