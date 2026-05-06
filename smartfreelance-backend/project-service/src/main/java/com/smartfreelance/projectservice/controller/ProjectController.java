58d1193f2360255979fe752cb0a52bfae33cdee4
Roua <roua.menaa@gmail.com>
2026-05-06 15:22:46 +0100

    run CI proj-svc

--- smartfreelance-backend/project-service/src/main/java/com/smartfreelance/projectservice/controller/ProjectController.java
+++ smartfreelance-backend/project-service/src/main/java/com/smartfreelance/projectservice/controller/ProjectController.java
@@ -8,7 +8,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
-
+//zerty
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
