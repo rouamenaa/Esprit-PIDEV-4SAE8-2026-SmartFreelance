a7908daf97b19c6e709838cce2a797a3f6eb03a2
Roua <roua.menaa@gmail.com>
2026-05-07 09:37:02 +0100

    apply local changes after rebase

--- smartfreelance-frontend/src/app/services/formation.service.spec.ts
+++ smartfreelance-frontend/src/app/services/formation.service.spec.ts
@@ -1,4 +1,4 @@
-﻿import { TestBed } from '@angular/core/testing';
+﻿﻿﻿﻿import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
