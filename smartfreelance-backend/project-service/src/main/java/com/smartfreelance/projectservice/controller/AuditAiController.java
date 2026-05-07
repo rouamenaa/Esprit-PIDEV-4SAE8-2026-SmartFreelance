7f347c021a57232f5b8eb75396a7f5307800415c
Roua <roua.menaa@gmail.com>
2026-05-07 09:33:47 +0100

    proj CI

--- smartfreelance-backend/project-service/src/main/java/com/smartfreelance/projectservice/controller/AuditAiController.java
+++ smartfreelance-backend/project-service/src/main/java/com/smartfreelance/projectservice/controller/AuditAiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/audit-ai")
 public class AuditAiController {
-
+    // azertyu
     private final AuditAiService aiService;
 
     public AuditAiController(AuditAiService aiService) {
